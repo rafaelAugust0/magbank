52fd74f3039e2ceb3eb6567560a76adcc0990267
rafaelAugust0 <rafael.augustoo1612@gmail.com>
2022-10-20 18:43:43 -0300

    including SASS/React-Bootstrap/Bootstrap (initial navbar structure)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
-import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Navbar />
     </div>
   );
 }
