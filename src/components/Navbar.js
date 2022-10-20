e10d57574c482be19cd38e719f6d2ac3e397c5f1
rafaelAugust0 <rafael.augustoo1612@gmail.com>
2022-10-20 20:12:03 -0300

    styling magbank's content (navbar)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,34 +1,44 @@
 import React from "react";
-import {Navbar, Nav, Button, ButtonGroup, NavDropdown} from "react-bootstrap";
+import { Container, Navbar, Nav, Button, ButtonGroup, NavDropdown } from "react-bootstrap";
 import './Navbar.scss';
+import logo from '../assets/logo.svg';
 
 const Navigation = () => (
-        <Navbar bg="light" expand="lg">
-            <Navbar.Brand href="#home">Magbank</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-                <Nav.Link href="#cartao">Cartão</Nav.Link>
-                <Nav.Link href="#quemsomos">Quem Somos</Nav.Link>
-                <Nav.Link href="#faq">FAQ</Nav.Link>
-            </Nav>
-            <ButtonGroup aria-label="Basic example">
-            <Button variant="outline-secondary">
-                <NavDropdown title="Acessar minha conta" id="basic-nav-dropdown">
-                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.2">
-                        Another action
-                    </NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action/3.4">
-                        Separated link
-                    </NavDropdown.Item>
-                </NavDropdown>
-            </Button>
-                <Button variant="outline-secondary">Abra sua conta</Button>
-            </ButtonGroup>
-            </Navbar.Collapse>
+        <Navbar variant="dark" expand="lg">
+            <Container>
+                <Navbar.Brand href="#home">
+                <img
+                    src= {logo}
+                    height="30"
+                    className="d-inline-block align-top"
+                    alt="Magbank logo"
+                />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                    <Nav.Link href="#cartao">Cartão</Nav.Link>
+                    <Nav.Link href="#quemsomos">Quem Somos</Nav.Link>
+                    <Nav.Link href="#faq">FAQ</Nav.Link>
+                </Nav>
+                <ButtonGroup aria-label="Basic example">
+                <Button variant="outline-light">
+                    <NavDropdown title="Acessar minha conta" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.2">
+                            Another action
+                        </NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action/3.4">
+                            Separated link
+                        </NavDropdown.Item>
+                    </NavDropdown>
+                </Button>
+                    <Button variant="outline-light">Abra sua conta</Button>
+                </ButtonGroup>
+                </Navbar.Collapse>
+            </Container>
         </Navbar>
 );
 
