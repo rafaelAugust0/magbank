164f08b934cea3d6c74795b995726e686df98115
rafaelAugust0 <rafael.augustoo1612@gmail.com>
2022-10-21 16:48:05 -0300

    including assets and styling credit card (section)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from './components/Hero';
+import CreditCard from './components/CreditCard';
 import "./App.scss";
 
 const App = () => (
     <div className="App">
       <Navbar />
       <Hero />
+      <CreditCard />
     </div>
   );
 
