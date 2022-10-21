f23c300d6556e62926e7c52b7268df1908d6cc64
rafaelAugust0 <rafael.augustoo1612@gmail.com>
2022-10-21 15:19:57 -0300

    styling magbank's content (hero)

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Button, Image, Row, Col } from 'react-bootstrap';
-import logo from '../assets/logo__magic-pay.png';
+import logo from '../assets/logo__magic-pay--mobile.png';
+import logoDesktop from '../assets/logo__magic-pay.png';
+import './Hero.scss';
 
 const Hero = () => (
-    <div class='jumbotron' className='text-center text-light'>
-        <Row className='my-lg-5'>
+    <div className=' jumbotron text-center text-light hero py-5'>
+        <Row className='py-lg-5'>
             <Col lg className='text-lg-right my-lg-5'>
-                <Image src={logo} />
+                <Image className='d-lg-none' src={logo} />
+                <Image className='d-none d-lg-inline-block' src={logoDesktop} />
             </Col>
 
-            <Col lg className='text-lg-left my-lg-5'>
+            <Col lg className='text-lg-left my-lg-5 p-lg-5'>
                 <p>Pague suas contas pelo nosso APP</p>
                 <Button variant="outline-light">
                     Abra sua conta
