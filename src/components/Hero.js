4dff4e81a79bfdc093bd0cbe55400b6b3cc01841
rafaelAugust0 <rafael.augustoo1612@gmail.com>
2022-10-21 14:32:01 -0300

    using bootstrap utilities (hero)

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import { Button, Image } from 'react-bootstrap';
+import { Button, Image, Row, Col } from 'react-bootstrap';
 import logo from '../assets/logo__magic-pay.png';
 
 const Hero = () => (
-    <div class='jumbotron' className='text-center'>
-        <Image src={logo} />
-            <p>Pague suas contas pelo nosso APP</p>
-            <Button variant="outline-light">
-                Abra sua conta
-            </Button>
+    <div class='jumbotron' className='text-center text-light'>
+        <Row className='my-lg-5'>
+            <Col lg className='text-lg-right my-lg-5'>
+                <Image src={logo} />
+            </Col>
+
+            <Col lg className='text-lg-left my-lg-5'>
+                <p>Pague suas contas pelo nosso APP</p>
+                <Button variant="outline-light">
+                    Abra sua conta
+                </Button>
+            </Col>
+        </Row>
     </div>
 );
 
