c48ca9e75c96e98888b942ed4147e7ef1e213b24
rafaelAugust0 <rafael.augustoo1612@gmail.com>
2022-10-28 10:37:44 -0300

    initial style content (FAQ)

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -28,58 +28,83 @@ import {
     }
     function FAQ() {
     return (
-        <Container>
-        <Row>
-            <Col className="d-lg-none" xs={12}>
+        <Container className="py-5 ">
+        <Row className="d-flex align-items-center justify-content-center">
+            <Col className="d-lg-none mb-5" xs={12}>
             <Row>
-                <Col>
+                <Col className="d-flex align-items-center justify-content-center">
                 <FontAwesomeIcon icon={faCreditCard} size="2x" />
                 </Col>
-                <Col>
+                <Col className="d-flex align-items-center justify-content-center">
                 <FontAwesomeIcon icon={faWallet} size="2x" />
                 </Col>
-                <Col>
+                <Col className="d-flex align-items-center justify-content-center">
                 <FontAwesomeIcon icon={faShieldAlt} size="2x" />
                 </Col>
-                <Col>
+                <Col className="d-flex align-items-center justify-content-center">
                 <FontAwesomeIcon icon={faUserTie} size="2x" />
                 </Col>
             </Row>
             </Col>
 
             <Col className="d-none d-lg-block">
-            <IconText icon={faCreditCard} size={3}>
+            <IconText icon={faCreditCard} size={3} className="mb-3" textClassName="lead">
                 Cartão de crédito e débito
             </IconText>
-            <IconText icon={faWallet} size={3}>
+            <IconText icon={faWallet} size={3} className="mb-3" textClassName="lead">
                 Conta e abertura
             </IconText>
-            <IconText icon={faShieldAlt} size={3}>
+            <IconText icon={faShieldAlt} size={3} className="mb-3" textClassName="lead">
                 Token digital
             </IconText>
-            <IconText icon={faUserTie} size={3}>
+            <IconText icon={faUserTie} size={3} textClassName="lead">
                 Produtos e serviços
             </IconText>
             </Col>
 
+            <Col>
             <Accordion defaultActiveKey="0">
             <Card>
                 <Card.Header>
-                <CustomToggle eventKey="0">Click me!</CustomToggle>
+                <CustomToggle eventKey="0">Lorem ipsum dolor sit amet</CustomToggle>
                 </Card.Header>
                 <Accordion.Collapse eventKey="0">
-                <Card.Body>Hello! I'm the body</Card.Body>
+                <Card.Body>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tincidunt arcu vel arcu fermentum, eget accumsan dolor dignissim. Nam eget quam semper, varius ligula sed, auctor augue. Maecenas et porta magna. Nulla mattis tortor vitae metus vestibulum scelerisque. Ut mollis a nisl vel suscipit. Curabitur scelerisque placerat ultrices. Proin purus ante, congue vel arcu ac, porta condimentum mauris. Maecenas massa orci, fringilla eget fermentum nec, dignissim nec diam.
+                </Card.Body>
                 </Accordion.Collapse>
             </Card>
+
             <Card>
                 <Card.Header>
-                <CustomToggle eventKey="1">Click me!</CustomToggle>
+                <CustomToggle eventKey="1">Nulla porttitor eros ac ex tristique posuere</CustomToggle>
                 </Card.Header>
                 <Accordion.Collapse eventKey="1">
-                <Card.Body>Hello! I'm another body</Card.Body>
+                <Card.Body>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tincidunt arcu vel arcu fermentum, eget accumsan dolor dignissim. Nam eget quam semper, varius ligula sed, auctor augue. Maecenas et porta magna. Nulla mattis tortor vitae metus vestibulum scelerisque. Ut mollis a nisl vel suscipit. Curabitur scelerisque placerat ultrices. Proin purus ante, congue vel arcu ac, porta condimentum mauris. Maecenas massa orci, fringilla eget fermentum nec, dignissim nec diam.
+                </Card.Body>
+                </Accordion.Collapse>
+            </Card>
+
+            <Card>
+                <Card.Header>
+                <CustomToggle eventKey="2">Quisque fringilla tincidunt arcu</CustomToggle>
+                </Card.Header>
+                <Accordion.Collapse eventKey="2">
+                <Card.Body>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tincidunt arcu vel arcu fermentum, eget accumsan dolor dignissim. Nam eget quam semper, varius ligula sed, auctor augue. Maecenas et porta magna. Nulla mattis tortor vitae metus vestibulum scelerisque. Ut mollis a nisl vel suscipit. Curabitur scelerisque placerat ultrices. Proin purus ante, congue vel arcu ac, porta condimentum mauris. Maecenas massa orci, fringilla eget fermentum nec, dignissim nec diam.
+                </Card.Body>
+                </Accordion.Collapse>
+            </Card>
+
+            <Card>
+                <Card.Header>
+                <CustomToggle eventKey="3">Nam elementum lobortis purus a maximus tortor</CustomToggle>
+                </Card.Header>
+                <Accordion.Collapse eventKey="3">
+                <Card.Body>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tincidunt arcu vel arcu fermentum, eget accumsan dolor dignissim. Nam eget quam semper, varius ligula sed, auctor augue. Maecenas et porta magna. Nulla mattis tortor vitae metus vestibulum scelerisque. Ut mollis a nisl vel suscipit. Curabitur scelerisque placerat ultrices. Proin purus ante, congue vel arcu ac, porta condimentum mauris. Maecenas massa orci, fringilla eget fermentum nec, dignissim nec diam.
+                </Card.Body>
                 </Accordion.Collapse>
             </Card>
             </Accordion>
+            </Col>
         </Row>
         </Container>
     );
