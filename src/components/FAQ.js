4b4bb7d88be75164ec71347399ac82ac49ffbc95
rafaelAugust0 <rafael.augustoo1612@gmail.com>
2022-10-28 15:20:00 -0300

    final style content and scss style (FAQ)

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -11,6 +11,9 @@ import {
     } from "@fortawesome/free-solid-svg-icons";
     import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+    import './FAQ.scss'
+
+
     function CustomToggle({ children, eventKey }) {
     const decoratedOnClick = useAccordionButton(eventKey, () =>
         console.log("totally custom!")
@@ -18,8 +21,9 @@ import {
 
     return (
         <button
+        class="btn btn-link"
         type="button"
-        style={{ backgroundColor: "pink" }}
+        style={{ backgroundColor: "unset" }}
         onClick={decoratedOnClick}
         >
         {children}
@@ -28,85 +32,88 @@ import {
     }
     function FAQ() {
     return (
-        <Container className="py-5 ">
-        <Row className="d-flex align-items-center justify-content-center">
-            <Col className="d-lg-none mb-5" xs={12}>
-            <Row>
-                <Col className="d-flex align-items-center justify-content-center">
-                <FontAwesomeIcon icon={faCreditCard} size="2x" />
-                </Col>
-                <Col className="d-flex align-items-center justify-content-center">
-                <FontAwesomeIcon icon={faWallet} size="2x" />
-                </Col>
-                <Col className="d-flex align-items-center justify-content-center">
-                <FontAwesomeIcon icon={faShieldAlt} size="2x" />
-                </Col>
-                <Col className="d-flex align-items-center justify-content-center">
-                <FontAwesomeIcon icon={faUserTie} size="2x" />
-                </Col>
-            </Row>
-            </Col>
+        <section className="faq text-light">
+            <Container className="py-5">
+                <h2 className="my-5 faq-title"> Dúvidas frequentes</h2>
+                <Row className="d-flex align-items-center justify-content-center">
+                    <Col className="d-lg-none mb-5" xs={12}>
+                    <Row>
+                        <Col className="d-flex align-items-center justify-content-center">
+                        <FontAwesomeIcon icon={faCreditCard} size="2x" />
+                        </Col>
+                        <Col className="d-flex align-items-center justify-content-center">
+                        <FontAwesomeIcon icon={faWallet} size="2x" />
+                        </Col>
+                        <Col className="d-flex align-items-center justify-content-center">
+                        <FontAwesomeIcon icon={faShieldAlt} size="2x" />
+                        </Col>
+                        <Col className="d-flex align-items-center justify-content-center">
+                        <FontAwesomeIcon icon={faUserTie} size="2x" />
+                        </Col>
+                    </Row>
+                    </Col>
 
-            <Col className="d-none d-lg-block">
-            <IconText icon={faCreditCard} size={3} className="mb-3" textClassName="lead">
-                Cartão de crédito e débito
-            </IconText>
-            <IconText icon={faWallet} size={3} className="mb-3" textClassName="lead">
-                Conta e abertura
-            </IconText>
-            <IconText icon={faShieldAlt} size={3} className="mb-3" textClassName="lead">
-                Token digital
-            </IconText>
-            <IconText icon={faUserTie} size={3} textClassName="lead">
-                Produtos e serviços
-            </IconText>
-            </Col>
+                    <Col className="d-none d-lg-block">
+                    <IconText icon={faCreditCard} size={3} className="mb-3" textClassName="lead" color="#fff">
+                        Cartão de crédito e débito
+                    </IconText>
+                    <IconText icon={faWallet} size={3} className="mb-3" textClassName="lead" color="#fff">
+                        Conta e abertura
+                    </IconText>
+                    <IconText icon={faShieldAlt} size={3} className="mb-3" textClassName="lead" color="#fff">
+                        Token digital
+                    </IconText>
+                    <IconText icon={faUserTie} size={3} textClassName="lead" color="#fff">
+                        Produtos e serviços
+                    </IconText>
+                    </Col>
 
-            <Col>
-            <Accordion defaultActiveKey="0">
-            <Card>
-                <Card.Header>
-                <CustomToggle eventKey="0">Lorem ipsum dolor sit amet</CustomToggle>
-                </Card.Header>
-                <Accordion.Collapse eventKey="0">
-                <Card.Body>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tincidunt arcu vel arcu fermentum, eget accumsan dolor dignissim. Nam eget quam semper, varius ligula sed, auctor augue. Maecenas et porta magna. Nulla mattis tortor vitae metus vestibulum scelerisque. Ut mollis a nisl vel suscipit. Curabitur scelerisque placerat ultrices. Proin purus ante, congue vel arcu ac, porta condimentum mauris. Maecenas massa orci, fringilla eget fermentum nec, dignissim nec diam.
-                </Card.Body>
-                </Accordion.Collapse>
-            </Card>
+                    <Col>
+                    <Accordion defaultActiveKey="0">
+                    <Card className="faq-card">
+                        <Card.Header>
+                        <CustomToggle eventKey="0">Lorem ipsum dolor sit amet</CustomToggle>
+                        </Card.Header>
+                        <Accordion.Collapse eventKey="0">
+                        <Card.Body>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tincidunt arcu vel arcu fermentum, eget accumsan dolor dignissim. Nam eget quam semper, varius ligula sed, auctor augue. Maecenas et porta magna. Nulla mattis tortor vitae metus vestibulum scelerisque. Ut mollis a nisl vel suscipit. Curabitur scelerisque placerat ultrices. Proin purus ante, congue vel arcu ac, porta condimentum mauris. Maecenas massa orci, fringilla eget fermentum nec, dignissim nec diam.
+                        </Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
 
-            <Card>
-                <Card.Header>
-                <CustomToggle eventKey="1">Nulla porttitor eros ac ex tristique posuere</CustomToggle>
-                </Card.Header>
-                <Accordion.Collapse eventKey="1">
-                <Card.Body>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tincidunt arcu vel arcu fermentum, eget accumsan dolor dignissim. Nam eget quam semper, varius ligula sed, auctor augue. Maecenas et porta magna. Nulla mattis tortor vitae metus vestibulum scelerisque. Ut mollis a nisl vel suscipit. Curabitur scelerisque placerat ultrices. Proin purus ante, congue vel arcu ac, porta condimentum mauris. Maecenas massa orci, fringilla eget fermentum nec, dignissim nec diam.
-                </Card.Body>
-                </Accordion.Collapse>
-            </Card>
+                    <Card className="faq-card">
+                        <Card.Header>
+                        <CustomToggle eventKey="1">Nulla porttitor eros ac ex tristique posuere</CustomToggle>
+                        </Card.Header>
+                        <Accordion.Collapse eventKey="1">
+                        <Card.Body>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tincidunt arcu vel arcu fermentum, eget accumsan dolor dignissim. Nam eget quam semper, varius ligula sed, auctor augue. Maecenas et porta magna. Nulla mattis tortor vitae metus vestibulum scelerisque. Ut mollis a nisl vel suscipit. Curabitur scelerisque placerat ultrices. Proin purus ante, congue vel arcu ac, porta condimentum mauris. Maecenas massa orci, fringilla eget fermentum nec, dignissim nec diam.
+                        </Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
 
-            <Card>
-                <Card.Header>
-                <CustomToggle eventKey="2">Quisque fringilla tincidunt arcu</CustomToggle>
-                </Card.Header>
-                <Accordion.Collapse eventKey="2">
-                <Card.Body>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tincidunt arcu vel arcu fermentum, eget accumsan dolor dignissim. Nam eget quam semper, varius ligula sed, auctor augue. Maecenas et porta magna. Nulla mattis tortor vitae metus vestibulum scelerisque. Ut mollis a nisl vel suscipit. Curabitur scelerisque placerat ultrices. Proin purus ante, congue vel arcu ac, porta condimentum mauris. Maecenas massa orci, fringilla eget fermentum nec, dignissim nec diam.
-                </Card.Body>
-                </Accordion.Collapse>
-            </Card>
+                    <Card className="faq-card">
+                        <Card.Header>
+                        <CustomToggle eventKey="2">Quisque fringilla tincidunt arcu</CustomToggle>
+                        </Card.Header>
+                        <Accordion.Collapse eventKey="2">
+                        <Card.Body>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tincidunt arcu vel arcu fermentum, eget accumsan dolor dignissim. Nam eget quam semper, varius ligula sed, auctor augue. Maecenas et porta magna. Nulla mattis tortor vitae metus vestibulum scelerisque. Ut mollis a nisl vel suscipit. Curabitur scelerisque placerat ultrices. Proin purus ante, congue vel arcu ac, porta condimentum mauris. Maecenas massa orci, fringilla eget fermentum nec, dignissim nec diam.
+                        </Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
 
-            <Card>
-                <Card.Header>
-                <CustomToggle eventKey="3">Nam elementum lobortis purus a maximus tortor</CustomToggle>
-                </Card.Header>
-                <Accordion.Collapse eventKey="3">
-                <Card.Body>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tincidunt arcu vel arcu fermentum, eget accumsan dolor dignissim. Nam eget quam semper, varius ligula sed, auctor augue. Maecenas et porta magna. Nulla mattis tortor vitae metus vestibulum scelerisque. Ut mollis a nisl vel suscipit. Curabitur scelerisque placerat ultrices. Proin purus ante, congue vel arcu ac, porta condimentum mauris. Maecenas massa orci, fringilla eget fermentum nec, dignissim nec diam.
-                </Card.Body>
-                </Accordion.Collapse>
-            </Card>
-            </Accordion>
-            </Col>
-        </Row>
-        </Container>
+                    <Card className="faq-card">
+                        <Card.Header>
+                        <CustomToggle eventKey="3">Nam elementum lobortis purus a maximus tortor</CustomToggle>
+                        </Card.Header>
+                        <Accordion.Collapse eventKey="3">
+                        <Card.Body>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tincidunt arcu vel arcu fermentum, eget accumsan dolor dignissim. Nam eget quam semper, varius ligula sed, auctor augue. Maecenas et porta magna. Nulla mattis tortor vitae metus vestibulum scelerisque. Ut mollis a nisl vel suscipit. Curabitur scelerisque placerat ultrices. Proin purus ante, congue vel arcu ac, porta condimentum mauris. Maecenas massa orci, fringilla eget fermentum nec, dignissim nec diam.
+                        </Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
+                    </Accordion>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
     );
 }
 export default FAQ;
